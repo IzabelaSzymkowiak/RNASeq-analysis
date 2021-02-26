d75be63937130457d1b051661d8e05bac4cd647d
IzabelaSzymkowiak <79699094+IzabelaSzymkowiak@users.noreply.github.com>
2021-02-26 14:05:09 +0100

    Add files via upload

---
+++ HisatSE.sh
@@ -0,0 +1,6 @@
+﻿#!/bin/bash
+for num in `seq 28 31`
+do
+ echo SRR31944${num}
+ hisat2 -q -x /home/iza/Pobrane/index/index -U /home/iza/Pulpit/fasq-gzip/TRIMM/SINGLE/sample_SRR31944${num}.fq.gz -S wynik_SRR31944${num}.hisat.sam
+done
